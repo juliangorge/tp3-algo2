dd5c5186e493c9d0dc91d50a9b5fe9845f95e28c
Julián <juliangorge@hotmail.com>
2021-12-01 06:14:04 -0300

    Subo Casilleros y Edificio

---
+++ CasilleroMuelle.h
@@ -0,0 +1,13 @@
+#ifndef CASILLEROMUELLE_H
+#define CASILLEROMUELLE_H
+
+#include "Casillero.h"
+
+class CasilleroMuelle : public Casillero
+{
+    public:
+        CasilleroLago();
+        ~CasilleroLago();
+};
+
+#endif //CASILLEROMUELLE_H
