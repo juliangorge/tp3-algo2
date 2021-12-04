7960297016ed6a4b2bf0dfffba0e9d538231cf64
nachoechegaray95 <iechegaray@fi.uba.ar>
2021-12-04 15:43:05 -0300

    agrego objetivos

---
+++ Edificio_mina.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "Edificio_mina.h"
+#include "Material.h"
+
+using namespace std;
+
+Mina::Mina()
+{
+    this->caracter =  'M';
+    this->nombre = "mina";
+}
+
+void Mina::recolectar_materiales(Material **lista_mats, int cantidad_mats_lista)
+{
+    sumar_materiales("piedra",15,lista_mats, cantidad_mats_lista);
+}
---
+++ Edificio_mina.h
@@ -0,0 +1,24 @@
+#ifndef EDIFICIO_MINA_H
+#define EDIFICII_MINA_H
+
+#include "Edificio.h"
+
+#include <string>
+
+using namespace std;
+
+class Mina: public Edificio
+{
+    public:
+    // Constructor
+    // PRE: -
+    // POS: crea un objeto del tipo escuela
+    Mina();
+
+    // PRE: -
+    // POS: devuelve al inventario la cantidad de materiales correspondiente a este objeto
+    void recolectar_materiales(Material **lista_mats, int cantidad_mats_lista);
+
+};
+
+#endif
